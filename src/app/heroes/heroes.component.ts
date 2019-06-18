2157fdb7a26f96bf122f7bab1ff37144c6ff48a2
Mike Richard <mrichard@ea.com>
2019-06-18 16:06:25 -0700

    Completed the routing tutorial.  Page has navigation now.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,11 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(
     private heroService: HeroService
-  ) {
-    this.selectedHero = null;
-  }
+  ) {}
 
   ngOnInit() {
     this.getHeroes();
@@ -24,10 +21,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe( heroes => this.heroes = heroes );
-  }
-
-  onSelect( hero: Hero ): void {
-    this.selectedHero = hero;
   }
 
 }
