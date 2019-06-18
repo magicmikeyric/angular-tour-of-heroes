9e319e548017ed66fc35cad39d451c950a600162
Mike Richard <mrichard@ea.com>
2019-06-18 15:18:00 -0700

    Completed Services.  Added a Hero Service to get hero data.  Added a Message service to get the list of hero messages.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +9,21 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() {
+  constructor(
+    private heroService: HeroService
+  ) {
     this.selectedHero = null;
   }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe( heroes => this.heroes = heroes );
   }
 
   onSelect( hero: Hero ): void {
